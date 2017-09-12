e723db0139ec2e3e0da35c528d8ab46fb8c19dc5
hdshiva <hdshiva@gmail.com>
2017-09-12 12:42:42 -0700

    change

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -8,7 +8,7 @@ def call(body) {
 
     // now build, based on the configuration provided
     node {
-        echo ${config.fullurl}
+        echo "${config.fullurl}"
         echo "user name is ${config.uid}"
 	echo "group name is : ${config.groupid}"
     }
